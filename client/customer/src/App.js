9071ca73dafd4948458c4257f36588830db276ca
trieuphan0211 <trieu.207ct28659@vanlanguni.vn>
2023-10-24 12:00:09 +0700

    first commit

---
+++ client/customer/src/App.js
@@ -0,0 +1,28 @@
+//CLI: npm install axios --save
+import axios from "axios";
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: "Loading...",
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h2>Admin page</h2>
+        <p>{this.state.message}</p>
+      </div>
+    );
+  }
+  componentDidMount() {
+    axios.get("/hello").then((res) => {
+      const result = res.data;
+      this.setState({ message: result.message });
+      console.log(result);
+    });
+  }
+}
+export default App;
